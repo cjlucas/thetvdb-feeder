71b5814ce12e71b6c3b2b6e36cbb83fdd0287b7a
Chris Lucas <chris@chrisjlucas.com>
2014-08-25 11:53:56 -0700

    Changed ical default for end offset

--- app/models/ical_settings.rb
+++ app/models/ical_settings.rb
@@ -1,7 +1,7 @@
 class IcalSettings < ActiveRecord::Base
   DEFAULTS = {
       start_offset: 0,
-      end_offset: 0,
+      end_offset: 5.years,
       timezone: 'America/New_York',
       adjust_airtime: false,
   }
