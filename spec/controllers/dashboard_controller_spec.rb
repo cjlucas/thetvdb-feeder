b73ae2172232d31bed974e5160e962ec82eef636
Chris Lucas <chris@chrisjlucas.com>
2014-08-26 09:49:27 -0700

    Added specs for dashboard controller

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe DashboardController, :type => :controller do
+describe DashboardController, :type => :controller do
+  context 'when accessing dashboard#index' do
+    it 'should redirect to login if user is not logged in' do
+      get :index
+      expect(response).to redirect_to(login_path)
+    end
 
+    it 'should redirect to login if session exists but cant be found in db' do
+      session[:uuid] = '85d5ff0e-1748-45e7-9017-7a9c909e6c03'
+      get :index
+      expect(response).to redirect_to(login_path)
+    end
+
+    it 'should respond with status code 200 if user is logged in' do
+      uuid = '85d5ff0e-1748-45e7-9017-7a9c909e6c03'
+      user = valid_user(uuid: uuid)
+      user.save
+      session[:uuid] = '85d5ff0e-1748-45e7-9017-7a9c909e6c03'
+
+      expect(response).to have_http_status(200)
+    end
+  end
 end
